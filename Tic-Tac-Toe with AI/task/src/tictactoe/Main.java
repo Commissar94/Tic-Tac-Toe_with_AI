38b7a3c676dae1950a25d2a63ef9fd3b0fc98579
Commissar94 <andreykomissarov94@gmail.com>
2021-07-09 15:16:00 +0300

    2 stage (easy AI)

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -1,20 +1,15 @@
 package tictactoe;
+
 import java.util.*;
 
 class ticTacToe {
 
     private char[][] grid = new char[3][3];
 
-    public void setGrid(String grid) {
-        int index = 0;
+    public void setGrid() {  //заполняем пустыми значениями
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (grid.charAt(index) == '_') {
-                    this.grid[i][j] = ' ';
-                } else {
-                    this.grid[i][j] = grid.charAt(index);
-                }
-                index++;
+                this.grid[i][j] = ' ';
             }
         }
     }
@@ -27,104 +22,122 @@ class ticTacToe {
         System.out.println("---------");
     }
 
-    public boolean fillGrid(int x, int y) {
+    public void playerMove(int x, int y) {
         if (x >= 1 && x <= 3 && y >= 1 && y <= 3) {
             if (this.grid[x - 1][y - 1] == ' ') {
-                int countX = 0;
-                int countO = 0;
-                for (int i = 0; i < 3; i++) { //считаем крестики и нолики
-                    for (int j = 0; j < 3; j++) {
-                        if (this.grid[i][j] == 'X') {
-                            countX++;
-                        } else if (this.grid[i][j] == 'O') {
-                            countO++;
-                        }
-                    }
-                }
-                if (countX == countO) { //если одинаково, то ходит Х
-                    this.grid[x - 1][y - 1] = 'X';
-                } else if (countX > countO) {
-                    this.grid[x - 1][y - 1] = 'O'; //если Х больше, то ходит 0
-                }
-                return false; //ход завершен
+                this.grid[x - 1][y - 1] = 'X';
             } else {
                 System.out.println("This cell is occupied! Choose another one!");
-                return true; //возвращаем правду, чтобы продолжать цикл в мейне
             }
         } else {
             System.out.println("Coordinates should be from 1 to 3!");
-            return true;
         }
     }
-    public void checkState() {
-        boolean isComplete = true;
-        boolean isWin = false;
-        for (int i = 0; i < 3; i++) {  //проверяем на ситуацию победы по горизонталям и вертикалям
+
+    public boolean checkWinX() {
+        for (int i = 0; i < 3; i++) {
+            //проверяем на ситуацию победы по горизонталям и вертикалям
             if (this.grid[i][0] == 'X' && this.grid[i][1] == 'X' && this.grid[i][2] == 'X') {
                 System.out.println("X wins");
-                isWin = true;
-                break;
-            } else if (this.grid[i][0] == 'O' && this.grid[i][1] == 'O' && this.grid[i][2] == 'O') {
-                System.out.println("O wins");
-                isWin = true;
-                break;
+                return true;
             } else if (this.grid[0][i] == 'X' && this.grid[1][i] == 'X' && this.grid[2][i] == 'X') {
                 System.out.println("X wins");
-                isWin = true;
-                break;
+                return true;
+            }
+        }
+        //если победы на прямых линиях не произошло, то смотрим диагонали
+        if (this.grid[0][0] == 'X' && this.grid[1][1] == 'X' && this.grid[2][2] == 'X') {
+            System.out.println("X wins");
+            return true;
+        } else if (this.grid[2][0] == 'X' && this.grid[1][1] == 'X' && this.grid[0][2] == 'X') {
+            System.out.println("X wins");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkWinO() {
+        for (int i = 0; i < 3; i++) {
+            //проверяем на ситуацию победы по горизонталям и вертикалям
+            if (this.grid[i][0] == 'O' && this.grid[i][1] == 'O' && this.grid[i][2] == 'O') {
+                System.out.println("O wins");
+                return true;
             } else if (this.grid[0][i] == 'O' && this.grid[1][i] == 'O' && this.grid[2][i] == 'O') {
                 System.out.println("O wins");
-                isWin = true;
-                break;
+                return true;
             }
         }
-        if (!isWin) {  //если победы на прямых линиях не произошло, то смотрим диагонали
-            if (this.grid[0][0] == 'X' && this.grid[1][1] == 'X' && this.grid[2][2] == 'X') {
-                System.out.println("X wins");
-            } else if (this.grid[0][0] == 'O' && this.grid[1][1] == 'O' && this.grid[2][2] == 'O') {
-                System.out.println("O wins");
-            } else if (this.grid[2][0] == 'X' && this.grid[1][1] == 'X' && this.grid[0][2] == 'X') {
-                System.out.println("X wins");
-            } else if (this.grid[2][0] == 'O' && this.grid[1][1] == 'O' && this.grid[0][2] == 'O') {
-                System.out.println("O wins");
-            } else {  //если и диагонали не выиграли, то проверяем есть ли еще пустые поля
-                for (int k = 0; k < 3; k++) {
-                    for (int l = 0; l < 3; l++) {
-                        if (this.grid[k][l] == ' ') {
-                            isComplete = false; //если есть, то выходит и говорим что игра не завершена
-                            break;
-                        }
-                    }
-                }
-                if (isComplete) { //если нет побед и пустых полей, то это ничья
-                    System.out.println("Draw");
-                } else { //ну а если нет побед, но есть пустые поля, то это незаконченная игра
-                    System.out.println("Game not finished");
-                }
-            }
+        //если победы на прямых линиях не произошло, то смотрим диагонали
+        if (this.grid[0][0] == 'O' && this.grid[1][1] == 'O' && this.grid[2][2] == 'O') {
+            System.out.println("O wins");
+            return true;
+        } else if (this.grid[2][0] == 'O' && this.grid[1][1] == 'O' && this.grid[0][2] == 'O') {
+            System.out.println("O wins");
+            return true;
         }
+        return false;
     }
+
+    public int findSpacesInGrid() {
+        int spaces = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (this.grid[i][j] == ' ') {
+                    spaces++;
+                }
+            }
+        }
+        return spaces;
+    }
+
+    public void aiEasyMove() {
+
+        System.out.println("Making move level \"easy\"");
+        Random random = new Random();
+        int x = random.nextInt(3) + 1;
+        int y = random.nextInt(3) + 1;
+        boolean moveSet = false;
+
+        while (!moveSet) {
+            if (this.grid[x - 1][y - 1] == ' ' && this.grid[x - 1][y - 1] != 'X') {
+                this.grid[x - 1][y - 1] = 'O';
+                moveSet = true;
+            } else if (this.grid[x - 1][y - 1] == 'X') {
+                x = random.nextInt(3) + 1;
+                y = random.nextInt(3) + 1;
+            }
+        }
+
+    }
+    
 }
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ticTacToe t1 = new ticTacToe();
-        System.out.print("Enter the cells: ");
-        t1.setGrid(scanner.nextLine());
+        t1.setGrid();
         t1.showGrid();
-        System.out.print("Enter the coordinates:");
-        boolean reRun = true;
         do {
             try {
-                reRun = t1.fillGrid(scanner.nextInt(), scanner.nextInt());
+                System.out.print("Enter the coordinates:");
+                t1.playerMove(scanner.nextInt(), scanner.nextInt());
+                t1.showGrid();
+                if (t1.checkWinX()) {
+                    break;
+                }
+                t1.aiEasyMove();
+                t1.showGrid();
             } catch (Exception e) {
                 System.out.println("You should enter numbers!");
                 System.out.print("Enter the coordinates:");
                 scanner.next();
             }
-        } while (reRun);
-        t1.showGrid();
-        t1.checkState();
+        } while (((!t1.checkWinO()) && (!t1.checkWinX())) && (t1.findSpacesInGrid() != 0));
+
+        if (t1.findSpacesInGrid() == 0) {
+            System.out.println("Draw");
+        }
     }
 }
